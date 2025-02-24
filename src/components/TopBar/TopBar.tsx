257a7c8c51278238f93f9e61b8b91f829095243f
TheWoxPL <wojtek.bubula@onet.pl>
2025-02-24 20:27:17 +0100

    feat: styled basic topbar

--- src/components/TopBar/TopBar.tsx
+++ src/components/TopBar/TopBar.tsx
@@ -5,13 +5,32 @@ export const TopBar = () => {
   return (
     <div className={styles.container}>
       <div className={styles.top}>
-        <div className={styles.location}>
+        <div className={styles.localization}>
           <span className={styles.title}>Location</span>
           <div className={styles.input}>choose</div>
         </div>
-        <img src={NotificationBellSVG} alt="" />
+        <img src={NotificationBellSVG} alt="notification svg" />
       </div>
-      <div className={styles.bottom}></div>
+      <div className={styles.bottom}>
+        <form>
+          <input
+            className={styles.search}
+            id="search"
+            type="text"
+            placeholder="search..."
+          />
+          <select
+            className={styles.options}
+            title="options"
+            name="options"
+            id="options"
+          >
+            <option value="val1" select-name="val1">
+              val1
+            </option>
+          </select>
+        </form>
+      </div>
     </div>
   );
 };
