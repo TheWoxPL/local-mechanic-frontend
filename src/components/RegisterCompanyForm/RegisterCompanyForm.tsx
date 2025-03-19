4eba47d3aa23d587bf679ffc20b10b8e7a116c67
TheWoxPL <wojtek.bubula@onet.pl>
2025-03-19 14:13:53 +0100

    feat: connect form with add company endpoint

--- src/components/RegisterCompanyForm/RegisterCompanyForm.tsx
+++ src/components/RegisterCompanyForm/RegisterCompanyForm.tsx
@@ -1,16 +1,45 @@
+import { FormEvent, useState } from 'react';
 import styles from './RegisterCompanyForm.module.scss';
+import ApiUtils from 'src/shared/api/apiUtils';
+import { CreateCompanyDTO } from 'src/shared/dtos';
 
 export const RegisterCompanyForm = () => {
+  const [companyName, setCompanyName] = useState('');
+  const [foundDate, setFoundDate] = useState(new Date());
+  const [description, setDescription] = useState('');
+  const [localization, setLocalization] = useState('');
+
+  const handleFormSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const data = new CreateCompanyDTO();
+    data.companyName = companyName;
+    data.description = description;
+    data.foundDate = foundDate;
+    ApiUtils.companies.addCompany(data);
+  };
+
   return (
     <div className={styles.container}>
-      <form>
+      <form onSubmit={(e) => handleFormSubmit(e)}>
         <div className={styles.inputField}>
           <label>Company name</label>
-          <input type="text" placeholder="your company inc." />
+          <input
+            type="text"
+            placeholder="your company inc."
+            value={companyName}
+            onChange={(e) => setCompanyName(e.target.value)}
+            required
+          />
         </div>
         <div className={styles.inputField}>
           <label>Found date</label>
-          <input type="text" placeholder="ex. 12.12.2012" />
+          <input
+            type="date"
+            placeholder="ex. 12.12.2012"
+            value={foundDate.toISOString().split('T')[0]}
+            onChange={(e) => setFoundDate(new Date(e.target.value))}
+          />
         </div>
         <div className={styles.ownersField}>
           <label>Owners:</label>
@@ -24,11 +53,18 @@ export const RegisterCompanyForm = () => {
             className={styles.descriptionTextArea}
             title="description"
             placeholder="type some information about your company..."
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
           ></textarea>
         </div>
         <div className={styles.inputField}>
           <label>Localization</label>
-          <input type="text" placeholder="cracow st. długa 12a" />
+          <input
+            type="text"
+            placeholder="cracow st. długa 12a"
+            value={localization}
+            onChange={(e) => setLocalization(e.target.value)}
+          />
         </div>
         <input type="submit" className={styles.submitButton} />
       </form>
